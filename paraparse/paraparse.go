e149f922c2e128f7010a55aaaae964ab38d6b029
Ben Turrubiates <ben@bturru.us>
2014-05-03 06:02:08 -0600

    MOD: Changed the output, added error handling, and wrote up some comments.

--- paraparse/paraparse.go
+++ paraparse/paraparse.go
@@ -1,44 +1,44 @@
 package paraparse
 
 import (
-    "github.com/skelterjohn/gopp"
-    "Paratype/context"
-    "strings"
-    "reflect"
-    "fmt"
+	"github.com/skelterjohn/gopp"
+	"Paratype/context"
+	"strings"
+	"reflect"
+	"fmt"
 )
 
 var _ = context.TypeClass{}
 
 type Base struct {
-    TypeclassDecls []TypeclassImpl
-    TypeDecls []Type
-    FuncDecls []Func
+	TypeclassDecls []TypeclassImpl
+	TypeDecls []Type
+	FuncDecls []Func
 }
 
 type Typeclass struct {
-    Name string
+	Name string
 }
 
 type TypeclassImpl struct {
-    Name Typeclass
-    Inherits []Typeclass
-    LastInherit Typeclass
+	Name Typeclass
+	Inherits []Typeclass
+	LastInherit Typeclass
 }
 
 type Type struct {
-    Name string
-    Implements []Typeclass
-    LastImplement Typeclass
+	Name string
+	Implements []Typeclass
+	LastImplement Typeclass
 }
 
 type Func struct {
-    Name string
+	Name string
 	Arguments []interface{}
 	LastArgument interface{}
 	ReturnType interface{}
-    Constraints []Constraint
-    LastConstraint Constraint
+	Constraints []Constraint
+	LastConstraint Constraint
 	Errors []Error
 	LastError Error
 	Expr
@@ -51,9 +51,9 @@ type Error struct {
 }
 
 type Constraint struct {
-    Name TypeVar
-    Tclasses []Typeclass
-    LastTClass Typeclass
+	Name TypeVar
+	Tclasses []Typeclass
+	LastTClass Typeclass
 }
 
 type TypeVar struct {
@@ -65,9 +65,9 @@ type TypeName struct {
 }
 
 type FuncCall struct {
-    Name string
-    Arguments []interface{}
-    LastArgument interface{}
+	Name string
+	Arguments []interface{}
+	LastArgument interface{}
 }
 
 
@@ -109,78 +109,100 @@ uident = /([N-Z][a-zA-Z]*)/
 typevar = /([A-M][a-zA-Z]*)/
 `
 func ParseCode() error {
-    df, err := gopp.NewDecoderFactory(paragopp, "Start")
-    if err != nil {
-        fmt.Println(err)
-        return err
-    }
+	df, err := gopp.NewDecoderFactory(paragopp, "Start")
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 	df.RegisterType(TypeVar{})
 	df.RegisterType(TypeName{})
 	df.RegisterType(Func{})
-    df.RegisterType(FuncCall{})
-    df.RegisterType(Error{})
-    df.RegisterType(Typeclass{})
-    df.RegisterType(Constraint{})
-    //dec := df.NewDecoder(strings.NewReader("typeclass Num\ntype y implements Zun, Num\ntype z implements Num\nfunc foo constrain A <Num, Zun> (d, A, y) iNT throws bigError, gError\n=x\n"))
+	df.RegisterType(FuncCall{})
+	df.RegisterType(Error{})
+	df.RegisterType(Typeclass{})
+	df.RegisterType(Constraint{})
+	//dec := df.NewDecoder(strings.NewReader("typeclass Num\ntype y implements Zun, Num\ntype z implements Num\nfunc foo constrain A <Num, Zun> (d, A, y) iNT throws bigError, gError\n=x\n"))
 	dec := df.NewDecoder(strings.NewReader("typeclass Num inherits Zin\ntypeclass Zin\ntype z\nfunc foo(d, A) iNT\n=x\n"))
-    out := &Base{}
+	out := &Base{}
 	err = dec.Decode(out)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	fmt.Printf("%#v\n", out)
-    fmt.Println(reflect.TypeOf(out.FuncDecls[0].LastError))
+	fmt.Println(reflect.TypeOf(out.FuncDecls[0].LastError))
 
-    fmt.Printf("\n\n\n")
-    PrintTypeclassDecls(out)
-    fmt.Printf("\n\n\n")
-    PrintTypeDecls(out)
-    fmt.Printf("\n\n\n")
-    PrintFuncDecls(out)
+	fmt.Printf("\n")
+	tlist, err := ParseTypeClassDecls(out)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Printf("%+v\n", tlist)
 
 
-
-    return err
+	return err
 }
 
-func PrintTypeclassDecls(data *Base) {
-    TypeclassList := make([]context.TypeClass, len(data.TypeclassDecls))
-    ReferenceMap := make(map[string]*context.TypeClass)
-    for i, elem := range data.TypeclassDecls {
-        TypeclassList[i].Name = elem.Name.Name
-        ReferenceMap[elem.Name.Name] = &TypeclassList[i]
-    }
-    for i, elem := range data.TypeclassDecls {
-        TypeclassList[i].Inherits = make(map[*context.TypeClass]bool)
-        for _, inherited := range elem.Inherits {
-            i_ref, ok := ReferenceMap[inherited.Name]
-            if !ok {
-                fmt.Printf("ERROR: %s does not exist.", inherited.Name)
-                return
-            }
-            TypeclassList[i].Inherits[i_ref] = true
-        }
-        if elem.LastInherit.Name != "" {
-            i_ref, ok := ReferenceMap[elem.LastInherit.Name]
-            if !ok {
-                fmt.Printf("ERROR: %s does not exist.", elem.LastInherit.Name)
-                //return
-            }
-            TypeclassList[i].Inherits[i_ref] = true
-        }
-        TypeclassList[i].Inherits[nil] = true
-    }
+// Once a paratype source file has been parsed it is contained in an object
+// of type base. This type contains a list that contains all of the type
+// classes in the source file and information about what they inherit. This
+// function will parse that list and place the resulting output into a
+// TypeClass object from the context package. This enables the paratype
+// type checker.
+func ParseTypeClassDecls(data *Base) ([]context.TypeClass, error) {
+	// The type context.TypeClass consists of two items:
+	// 		1) A Name
+	// 		2) A map of inherited TypeClasses where the key is a pointer to
+	// 			the TypeClass object and the value is a boolean representing
+	// 			the inheritance status.
+	//
+	TypeClassSlice := make([]context.TypeClass, len(data.TypeclassDecls))
+	ReferenceMap := make(map[string]*context.TypeClass)
+
+	// Copy the Name and fill the ReferenceMap entry with the appropriate
+	// pointer. This will be used to set the references later.
+	for i, elem := range data.TypeclassDecls {
+		TypeClassSlice[i].Name = elem.Name.Name
+		ReferenceMap[elem.Name.Name] = &TypeClassSlice[i]
+	}
+
+	// Go through the inherited TypeClasses and retrieve their references.
+	// The references are then used as a key in the Inherits map contained
+	// in the TypeClass struct. If retrieving the entry from the ReferenceMap
+	// fails this signifies a malformed source file. (References a TypeClass
+	// that does not exist.)
+	for i, elem := range data.TypeclassDecls {
+		TypeClassSlice[i].Inherits = make(map[*context.TypeClass]bool)
+		for _, inherited := range elem.Inherits {
+			i_ref, ok := ReferenceMap[inherited.Name]
+			if !ok {
+				return nil, fmt.Errorf("ParseTypeClassDecls: TypeClass %s does not exist.", inherited.Name)
+			}
+			TypeClassSlice[i].Inherits[i_ref] = true
+		}
+		if elem.LastInherit.Name != "" {
+			i_ref, ok := ReferenceMap[elem.LastInherit.Name]
+			if !ok {
+				return nil, fmt.Errorf("ParseTypeClassDecls: TypeClass %s does not exist.", elem.LastInherit.Name)
+			}
+			TypeClassSlice[i].Inherits[i_ref] = true
+		}
+		// This is for convention purposes. This will become more relevant
+		// when parsing types. Nil represents the basetype.
+		TypeClassSlice[i].Inherits[nil] = true
+	}
+	return TypeClassSlice, nil
 }
 
 func PrintTypeDecls(data *Base) {
-    for _, elem := range data.TypeDecls {
-        fmt.Printf("%+v\n", elem)
-    }
+	for _, elem := range data.TypeDecls {
+		fmt.Printf("%+v\n", elem)
+	}
 }
 
 func PrintFuncDecls(data *Base) {
-    for _, elem := range data.FuncDecls {
-        fmt.Printf("%+v\n", elem)
-    }
+	for _, elem := range data.FuncDecls {
+		fmt.Printf("%+v\n", elem)
+	}
 }
