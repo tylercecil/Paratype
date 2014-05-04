b41839456757f91b2ca85b9cc9aab1f3c865899b
Christopher Koch <ckoch@cs.nmt.edu>
2014-05-03 21:31:07 -0600

    MOD: renamed ConvertPath function, some testing facilities

--- paratype.go
+++ paratype.go
@@ -3,28 +3,23 @@ package main
 
 import (
 	"sync"
-	//"time"
 	"fmt"
-	//"math/rand"
 	"Paratype/context"
+	"strings"
 )
 
 // Object to represent a communication
 type Communication struct {
-	Path	string
-	Context	*context.Function
+	path	string
+	context	*context.Function
 }
 
 // Object to represent the Function as an actor.
 // This is probably bad design (we have structs everywhere!)
 // but will be fine for now.
 type FunctionActor struct {
-	Function	*context.Function
-	Channel		chan *Communication
-
-	// Temporary channel for testing
-	Tmp			chan string
-
+	function	*context.Function
+	channel		chan *Communication
 	state		bool
 	activeGroup	*sync.WaitGroup
 }
@@ -36,20 +31,29 @@ type FunctionActor struct {
 // By doing this, I am providing channels to all functions in
 // their run routines. I am not confident this is the best way
 // as of right now, but it will do.
-//const functionCount = 10
 var functions map[*context.Function]*FunctionActor
+// hackish.
+var functionsArray []*context.Function
 
 // A Functions main ruitine.
 func (f *FunctionActor) Run() {
 	f.makeActive(false)
-	for message := range f.Channel {
+	for message := range f.channel {
 
 		f.makeActive(true)
-		f.Function.Update(message.Context)
+		fmt.Printf("%v received from path ", f.function.Name)
+		pathfuncs := context.PathToFunctions(message.path, functionsArray)
+		s := make([]string, len(pathfuncs))
+		for i, g := range pathfuncs {
+			s[i] = g.Name
+		}
+		fmt.Printf("%s the context of %v\n", strings.Join(s, "-"), message.context.Name)
+		f.function.Update(message.context)
 
-		for _, gfuncs := range f.Function.Children {
+		message.path = context.AddToPath(message.path, f.function)
+		for _, gfuncs := range f.function.Children {
 			for g := range gfuncs {
-				functions[g].Channel <- message
+				functions[g].channel <- message
 			}
 		}
 		f.makeActive(false)
@@ -60,7 +64,6 @@ func (f *FunctionActor) Run() {
 // Change the state of the function actor. This is used
 // for the halting conditions.
 func (f *FunctionActor) makeActive(state bool) {
-
 	if state == f.state {
 		return
 	}
@@ -78,30 +81,45 @@ func (f *FunctionActor) Initialize(activeGroup *sync.WaitGroup) {
 	f.activeGroup = activeGroup
 	// Arbitrary buffer size. Note that channels block
 	// only when the buffer is full.
-	f.Channel = make(chan *Communication, 128)
-	f.Tmp = make(chan string, 128)
+	f.channel = make(chan *Communication, 128)
 	f.makeActive(true)
 }
 
 func (f *FunctionActor) SendToChild() {
 	comm := new(Communication)
-	comm.Path = context.ConvertPath(f.Function)
-	comm.Context = f.Function
-	for _, gfuncs := range f.Function.Children {
+	comm.path = context.FunctionsToPath(f.function)
+	comm.context = f.function
+	for _, gfuncs := range f.function.Children {
 		for g := range gfuncs {
-			functions[g].Channel <-comm
+			functions[g].channel <-comm
 		}
 	}
 }
 
-// not ready
+
+// given a list of functions, run everything!
 func RunThings(f ...interface{}) {
 
 	functions = make(map[*context.Function]*FunctionActor)
-	for _, fun := range f {
-		fActor := new(FunctionActor)
-		fActor.Function = fun.(*context.Function)
-		functions[fun.(*context.Function)] = fActor
+
+	// one can pass in multiple Function pointers or a slice of them
+	// tests are usually multiple while the parser will generate a slice
+	switch f[0].(type) {
+	case []*context.Function:
+		for _, fun := range f[0].([]*context.Function) {
+			fActor := new(FunctionActor)
+			fActor.function = fun
+			functions[fun] = fActor
+			functionsArray = append(functionsArray, fun)
+		}
+
+	case *context.Function:
+		for _, fun := range f {
+			fActor := new(FunctionActor)
+			fActor.function = fun.(*context.Function)
+			functions[fun.(*context.Function)] = fActor
+			functionsArray = append(functionsArray, fun.(*context.Function))
+		}
 	}
 
 	readyToFinish := new(sync.WaitGroup)
@@ -109,54 +127,32 @@ func RunThings(f ...interface{}) {
 	fmt.Println("Welcome to Paratype!")
 
 	for _, fActor := range functions {
-		fmt.Printf("\tSpawning Function Actor for %v\n", fActor.Function.Name)
 		fActor.Initialize(readyToFinish)
 	}
+	// avoid race conditions by having the first communication in channels
+	// before starting
 	for _, fActor := range functions {
 		fActor.SendToChild()
 	}
 	for _, fActor := range functions {
+		fmt.Printf("\tSpawning Function Actor for %v\n", fActor.function.Name)
 		go fActor.Run()
 	}
 
 	fmt.Println("Waiting for halting...")
+
 	// This is actually a race condition. It WOULD be sufficient
 	// to both make this check AND check if all channels are
 	// empty.
-
 	readyToFinish.Wait()
 	for _, fActor := range functions {
-		close(fActor.Channel)
-		close(fActor.Tmp)
+		// close channels, otherwise goroutines will hang
+		close(fActor.channel)
 	}
 
 	fmt.Println("Done!")
-
 }
 
 // Dummy main function.
 func main() {
-	// Make a set of junk functions
-	// Run all functions
-	// Wait to halt
-
-	//RunThings()
-
-	/*readyToFinish := new(sync.WaitGroup)
-
-	fmt.Println("Welcome to Paratype!")
-
-	for i, fActor := range functions {
-		fmt.Printf("\tSpawning %d Function Actor\n", i)
-		fActor.Initialize(readyToFinish)
-		go fActor.Run()
-	}
-
-	fmt.Println("Waiting for halting...")
-	// This is actually a race condition. It WOULD be sufficient
-	// to both make this check AND check if all channels are
-	// empty.
-	readyToFinish.Wait()
-	fmt.Println("Done!")*/
-
 }
