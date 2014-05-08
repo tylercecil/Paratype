1a4b175fef43489f6b1d7eb5de248e21076177d3
Ben Turrubiates <ben@bturru.us>
2014-05-08 02:27:48 -0600

    MOD: Generation working slightly better. Going to add children creation and nested children creation. Then just need to figure out how to compose that set.

--- generation/gen_tests.py
+++ generation/gen_tests.py
@@ -32,17 +32,18 @@ def test_type_var(var):
 def test_type_name(var):
     return re.match(r'[a-z][a-zA-Z]*', var)
 
-
 class FunctionObject:
-    def __init__(self, name, types):
+    def __init__(self, types, functions, autonomous=True):
         self.types = types
-        self.name = name
-        self.__fill_args()
-        self.__fill_return()
-        self.return_value = ""
+        self.name = gen_func_name()
+        if autonomous:
+            self.__fill_args()
+            self.__fill_return()
+            self.__complete_return(functions)
+        functions.append(self)
 
     def __fill_args(self):
-        self.argcount = random.randint(0, 3)
+        self.argcount = random.randint(1, 2)
         self.arglist = []
         for i in range(self.argcount):
             if random.randint(1, 10) <= 7:
@@ -51,75 +52,45 @@ class FunctionObject:
                 self.arglist.append(gen_type_var())
 
     def __fill_return(self):
-        if self.arglist:
-            #type_vars = [test_type_var(var) for var in self.arglist]
-            #if not any(type_vars):
-            #    return_pool = self.arglist + [gen_type_var()]
-            #else:
-            #    return_pool = self.arglist
-            self.return_type = random.choice(self.arglist)
-            return
-        self.return_type = random.choice(self.types)
-        self.return_value = self.return_type
+        self.return_type = random.choice(self.arglist)
 
-
-    def check_return(self, function):
-        if self.return_type == function.return_type:
-            return True
-        elif test_type_var(self.return_type):
-            return True
-        else:
-            return False
-
-    def check_compatibility(self, function):
-        func_params = [i for i in function.arglist if test_type_name(i)]
-        if self is function:
-            return False
-        elif set(self.arglist).issuperset(set(func_params)):
-            return True
-        else:
-            return False
-
-    def find_compatible_functions(self, functions):
-        funcs = [i for i in functions if self.check_return(i)]
-        funcs = [i for i in funcs if self.check_compatibility(i)]
-        return funcs
-
-    def assign_by_self(self, functions):
-        if test_type_name(self.return_type):
+    def __complete_return(self, functions):
+        next_move = random.randint(0, 3)
+        if next_move <= 3:
             self.return_value = self.return_type
-        else:
-            if self.arglist:
-                valid_values = [i for i in self.arglist if test_type_name(i)]
-                if(valid_values):
-                    self.return_value = random.choice(valid_values)
-                    return
+            self.generate_parent(functions)
 
-    def fill_type_vars(self, ret_string):
-        for i in re.findall("{.}", ret_string):
-            if self.arglist:
-                ret_string = ret_string.replace(i, random.choice(self.arglist))
-            else:
-                print("ERRORS")
-        return ret_string
-
-    def complete_return(self, functions):
-        if self.return_value:
-            return
+    def generate_parent(self, functions):
+        parent = FunctionObject(self.types, functions, False)
+        parent.argcount = random.randint(self.argcount, self.argcount+3)
+        new_args_list = self.arglist[:]
         if test_type_var(self.return_type):
-            funcs = self.find_compatible_functions(functions)
-            ret = random.choice(funcs).get_call_string()
-            ret = self.fill_type_vars(ret)
-            self.return_value = ret
+            try:
+                idx = new_args_list.index(self.return_type)
+                new_args_list[idx] = random.choice(self.types)
+                parent.return_type = new_args_list[idx]
+                parent.arglist = new_args_list[:]
+                if any(map(test_type_var, parent.arglist)):
+                    parent.generate_parent(functions)
+            except ValueError:
+                parent.return_type = random.choice(self.types)
+                for i, v in enumerate(new_args_list):
+                    if test_type_var(v):
+                        new_args_list[i] = random.choice(self.types)
+                        break
+                parent.arglist = new_args_list[:]
         else:
-            if random.randint(1, 10) >= 8:
-                funcs = self.find_compatible_functions(functions)
-                if funcs:
-                    ret = random.choice(funcs).get_call_string()
-                    ret = self.fill_type_vars(ret)
-                    self.return_value = ret
-                    return
-            self.return_value = self.return_type
+            parent.return_type = self.return_type
+            parent.arglist = new_args_list[:]
+            for i, v in enumerate(parent.arglist):
+                if test_type_var(v):
+                    parent.arglist[i] = random.choice(self.types)
+                    break
+        parent.return_value = self.name + '(' + ",".join(parent.arglist) + ')'
+        while len(parent.arglist) < parent.argcount:
+            parent.arglist.append(random.choice(self.types))
+        if any(map(test_type_var, parent.arglist)):
+            parent.generate_parent(functions)
 
     def __repr__(self):
         return "func {0}({1}) {2}\n={3}\n".format(self.name, ','.join(self.arglist), self.return_type, self.return_value)
@@ -140,13 +111,7 @@ class FunctionObject:
 
 random.seed()
 types = [gen_type_name(random.randint(3, 6)) for _ in range(3)]
-functions = [gen_func_name(random.randint(3, 6)) for _ in range(10)]
-function_list = [FunctionObject(i, types) for i in functions]
-
-for i in function_list:
-    i.complete_return(function_list)
-
-for i in types:
-    print("type {0}".format(i))
-for i in function_list:
-    print(i, end="")
+functions = []
+x = FunctionObject(types, functions)
+for i in functions:
+    print(i)
