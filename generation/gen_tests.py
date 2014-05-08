024cc8169400b71ea0d8ee5cc01b5a627d1afb1a
Ben Turrubiates <ben@bturru.us>
2014-05-07 21:30:33 -0600

    ADD: Tried to write a test generator. It's hastily made and generates stuff that may or may not be correct. I'll have to do more testing regarding that later...

---
+++ generation/gen_tests.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+import string
+import random
+import re
+
+def gen_string(regex=r'[a-z]', length=3):
+    full_alpha = string.ascii_uppercase + string.ascii_lowercase
+    first_pool = re.findall(regex, full_alpha)
+    first = [random.choice(first_pool)]
+    rest = [random.choice(full_alpha) for _ in range (length - 1)]
+    return ''.join(first + rest)
+
+def gen_type_name(t_length=3):
+    return gen_string(length=t_length)
+
+def gen_type_var(length=1):
+    return gen_string(r'[A-M]', length)
+
+def gen_typeclass_name(length=3):
+    return gen_string(r'[N-Z]', length)
+
+def gen_func_name(f_length=3):
+    return gen_string(length=f_length)
+
+def gen_error_name(e_length=3):
+    return gen_string(length=e_length)
+
+def test_type_var(var):
+    return re.match(r'[A-M][a-zA-Z]*', var)
+
+def test_type_name(var):
+    return re.match(r'[a-z][a-zA-Z]*', var)
+
+
+class FunctionObject:
+    def __init__(self, name, types):
+        self.types = types
+        self.name = name
+        self.__fill_args()
+        self.__fill_return()
+        self.return_value = ""
+
+    def __fill_args(self):
+        self.argcount = random.randint(0, 4)
+        self.arglist = []
+        for i in range(self.argcount):
+            if random.randint(1, 10) <= 7:
+                self.arglist.append(random.choice(self.types))
+            else:
+                self.arglist.append(gen_type_var())
+
+    def __fill_return(self):
+        if self.arglist:
+            type_vars = [test_type_var(var) for var in self.arglist]
+            if not any(type_vars):
+                return_pool = self.arglist + [gen_type_var()]
+            else:
+                return_pool = self.arglist
+            self.return_type = random.choice(return_pool)
+            return
+        self.return_type = random.choice(self.types)
+        self.return_val = self.return_type
+
+
+    def check_return(self, function):
+        if self.return_type == function.return_type:
+            return True
+        elif test_type_var(self.return_type):
+            return True
+        else:
+            return False
+
+    def check_compatibility(self, function):
+        func_params = [i for i in function.arglist if test_type_name(i)]
+        if self is function:
+            return False
+        elif set(self.arglist).issuperset(set(func_params)):
+            return True
+        else:
+            return False
+
+
+    def assign_by_self(self, functions):
+        if test_type_name(self.return_type):
+            self.return_value = self.return_type
+        else:
+            if self.arglist:
+                valid_values = [i for i in self.arglist if test_type_name(i)]
+                if(valid_values):
+                    self.return_value = random.choice(valid_values)
+                    return
+        self.return_value = random.choice(self.types)
+
+
+    def complete_return(self, functions):
+        if self.return_value:
+            return
+        funcs = [i for i in functions if self.check_return(i)]
+        funcs2 = [i for i in funcs if self.check_compatibility(i)]
+        if funcs2:
+            if random.randint(1, 10) >= 7:
+                ret = random.choice(funcs2).get_call_string()
+                for i in re.findall("{.}", ret):
+                    if self.arglist:
+                        ret = ret.replace(i, random.choice(self.arglist))
+                    else:
+                        self.assign_by_self(functions)
+                self.return_value = ret
+                return
+        self.assign_by_self(functions)
+
+    def __repr__(self):
+        return "func {0}({1}) {2}\n={3}\n".format(self.name, ','.join(self.arglist), self.return_type, self.return_value)
+    def __str__(self):
+        return "func {0}({1}) {2}\n={3}\n".format(self.name, ','.join(self.arglist), self.return_type, self.return_value)
+
+    def get_call_string(self):
+        count = 0
+        ret = []
+        for i in self.arglist:
+            if test_type_var(i):
+                ret.append("{" + str(count) + "}")
+                count += 1
+            else:
+                ret.append(i)
+        func_call = self.name + '(' + ",".join(ret) + ')'
+        return func_call
+
+random.seed()
+types = [gen_type_name(random.randint(3, 6)) for _ in range(3)]
+functions = [gen_func_name(random.randint(3, 6)) for _ in range(10)]
+function_list = [FunctionObject(i, types) for i in functions]
+function_list
+for i in function_list:
+    i.complete_return(function_list)
+
+function_list
